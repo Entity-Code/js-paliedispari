9daef83de88a15bea982408de6d44f297c314217
Mattia <mattia.tummolilllo@gmail.com>
2020-10-27 14:57:24 +0100

    Completamento esercizio con logica adattata in pagina + controllo.

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,12 +1,19 @@
 //PALINDROMO
 
-// chiedo all'utente di inserire una parola
-var parola = prompt("Inserisci una parola");
-validationPalindrome(parola);
-console.log(parola);
+var invia = document.getElementById("invia");
+invia.addEventListener("click",
+	function() {
+      // chiedo all'utente di inserire una parola
+      var parola = document.getElementById("input").value;
 
-
-
+      // controllo inserimento parola
+      if (parola == "") {
+         document.getElementById("risultato").innerHTML = "Inserisci prima una parola, grazie";
+      } else {
+         validationPalindrome(parola);
+      }
+   }
+);
 
 
 
@@ -26,9 +33,12 @@ function reverseString(name) {
 
 //creo funzione per dire in output se la parola inserita è palindroma o no
 function validationPalindrome(string) {
+
    if (string === reverseString(string)) {
-      console.log("Questa parola è palindroma");
-   } else {
-      console.log("Questa parola non è palindroma");
+      return document.getElementById("risultato").innerHTML = "Questa parola è palindroma";
+   } else if (string !== reverseString(string)){
+      return document.getElementById("risultato").innerHTML = "Questa parola non è palindroma";
+   } else if (string == ""){
+      return document.getElementById("risultato").innerHTML = "Inserisci prima una parola, grazie";
    }
 }
